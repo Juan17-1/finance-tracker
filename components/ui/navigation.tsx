b42d8e71a88c3a4995ff257af675f9cf6463c349
Juan17-1 <142368277+Juan17-1@users.noreply.github.com>
2024-05-22 22:39:09 -0700

    update navigatino.tsx
    updates

--- components/ui/navigation.tsx
+++ components/ui/navigation.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { NavButton } from "@/components/ui/nav-button";
 import { useMedia } from "react-use";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
 
 const routes = [
   {
@@ -29,6 +32,28 @@ const routes = [
 ];
 
 export const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+  const isMobile = useMedia("(max-width: 1024px)", false);
+  const onClick = (href: string) => {
+    router.push(href);
+    setIsOpen(false);
+  };
+  if (isMobile) {
+    return (
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger>
+          <Button
+            variant="outline"
+            size="sm"
+            className="font-noraml bg-white/10 hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition"
+          >
+            <Menu className="h-4 w-4" />
+          </Button>
+        </SheetTrigger>
+      </Sheet>
+    );
+  }
   const pathname = usePathname();
   return (
     <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
