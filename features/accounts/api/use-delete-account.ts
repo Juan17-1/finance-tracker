e396320dcf76880e158bb19a50463d702188db89
Juan17-1 <142368277+Juan17-1@users.noreply.github.com>
2024-06-05 22:12:02 -0700

    progress save
    work in progress

--- features/accounts/api/use-delete-account.ts
+++ features/accounts/api/use-delete-account.ts
@@ -4,28 +4,26 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { client } from "@/lib/hono";
 
 type ResponseType = InferResponseType<
-  (typeof client.api.accounts)["bulk-delete"]["$post"]
+  (typeof client.api.accounts)[":id"]["$delete"]
 >;
-type RequestType = InferRequestType<
-  (typeof client.api.accounts)[":id"]["$post"]
->["json"];
 
-export const useBulkDeleteAccounts = () => {
+export const useDeleteAccount = (id?: string) => {
   const queryClient = useQueryClient();
-  const mutation = useMutation<ResponseType, Error, RequestType>({
-    mutationFn: async (json) => {
-      const response = await client.api.accounts["bulk-delete"]["$post"]({
-        json,
+  const mutation = useMutation<ResponseType, Error>({
+    mutationFn: async () => {
+      const response = await client.api.accounts[":json"]["$delete"]({
+        param: { id },
       });
       return await response.json();
     },
     onSuccess: () => {
       toast.success("Account deleted");
+      queryClient.invalidateQueries({ queryKey: ["account", { id }] });
       queryClient.invalidateQueries({ queryKey: ["accounts"] });
-      // TODO: Also invalidate summary
+      // TODO: Invalidate summary and transactions
     },
     onError: () => {
-      toast.error("Failed to delete accounts");
+      toast.error("Failed to delete account");
     },
   });
 
